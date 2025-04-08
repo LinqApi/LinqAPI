f9c2f40e1a23831815b455860c7b50d46d2331ba
Cihan Dalgicdir <cihandalgicdir@gmail.com>
2025-04-08 12:19:06 +0300

    swagger is removed from linqdynamicmvc

--- demo/LinqApi.Dynamic.Demo/Program.cs
+++ demo/LinqApi.Dynamic.Demo/Program.cs
@@ -5,8 +5,6 @@ using LinqApi.Dynamic.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDynamicLinqApi(string.Empty, "Data Source=.\\SQLEXPRESS;Database=ppp4;Trusted_Connection=True;TrustServerCertificate=Yes");
 
@@ -26,27 +24,13 @@ builder.Services.AddControllersWithViews()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// Statik dosyaları sunmak (CSS, JS, img vs.)
-app.UseStaticFiles();
-
 app.UseRouting();
 
-// Varsayılan route'u ayarlıyoruz, örneğin MSController'daki Index action'ı çalışacak.
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=LinqMvc}/{action=Index}/{id?}");
 
 
 _ = app.MapControllerRoute(
                name: "default",
-               pattern: "{controller=Home}/{action=Index}/{id?}",
+               pattern: "{controller=LinqDynamicMvc}/{action=Index}/{id?}",
                defaults: new { area = "" }
            );
 app.Run();
